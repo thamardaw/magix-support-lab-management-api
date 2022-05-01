f138b943d848ccfa7d20799b2d33ca9df5fa07ff
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-05-01 17:29:54 +0630

    added result_type and reault_default_text in parameter

--- core/entity/parameter.py
+++ core/entity/parameter.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
+from infrastructure.models.parameter import result_type_enum
 # from .lab_test import Lab_Test
 
 class Parameter(BaseModel):
@@ -9,6 +10,8 @@ class Parameter(BaseModel):
     unit : str
     lab_test_id : Optional[int] = None
     # lab_test : Optional[Lab_Test] = None
+    result_type : Optional[result_type_enum] = None
+    result_default_type : Optional[List[str]] = None
     created_time: Optional[datetime] = None
     updated_time: Optional[datetime] = None
     created_user_id: Optional[int] = None
--- infrastructure/models/parameter.py
+++ infrastructure/models/parameter.py
@@ -1,11 +1,18 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, JSON, Enum
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.orm import relationship
 from infrastructure.base_mixin import BaseMixin
 from infrastructure.base_class import Base
+import enum
+
+class result_type_enum(str,enum.Enum):
+    text = "text"
+    number = "number"
 
 class Parameter(BaseMixin,Base):
     name = Column(String, nullable=False)
     unit = Column(String, nullable=False)
     lab_test_id = Column(Integer,ForeignKey("lab_test.id"))
     lab_test = relationship("Lab_Test",backref="parameter")
+    result_type = Column(Enum(result_type_enum))
+    result_default_type = (JSON)
