c96fe68c483abad039b8df897c28fe10a27ce4d6
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-05-08 16:09:51 +0630

    added patient routes

--- apis/base.py
+++ apis/base.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from .routes import user,authentication
+from .routes import user,authentication,patient
 
 router = APIRouter(prefix="/api")
 
 router.include_router(user.router)
 router.include_router(authentication.router)
+router.include_router(patient.router)
---
+++ apis/routes/patient.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, Depends, status
+from schemas.message import Message
+from schemas.patient import Patient
+from core.services.patient import PatientService
+from core.entity.patient import Patient as PatientDTO
+from typing import List
+from infrastructure.repository.patient import PatientRepository
+
+router = APIRouter(prefix="/patients", tags=["Patients"])
+
+@router.get('/',status_code=status.HTTP_200_OK, response_model=List[PatientDTO])
+def get_all_patients(repo=Depends(PatientRepository)):
+    return PatientService(repo).getAllPatient()
+
+@router.get('/{id}',status_code=status.HTTP_200_OK,response_model=PatientDTO)
+def get_patient(id: int, repo=Depends(PatientRepository)):
+    return PatientService(repo).getPatient(id)
+
+@router.post("/", status_code=status.HTTP_200_OK, response_model=Message)
+def create(request: Patient, repo=Depends(PatientRepository)):
+    PatientService(repo).createPatient(request)
+    return {"detail": "Patient create successful."}
+
+@router.put("/{id}",status_code=status.HTTP_200_OK,response_model=Message)
+def update(id: int, request: Patient,repo=Depends(PatientRepository)):
+    PatientService(repo).updatePatient(id,request)
+    return {"detail": "Patient update successful."}
+
+@router.delete("/{id}",status_code=status.HTTP_200_OK, response_model=Message)
+def delete(id: int, repo=Depends(PatientRepository)):
+    PatientService(repo).deletePatient(id)
+    return {"detail": "Patient delete successful."}
+
+@router.post("/bulk",status_code=status.HTTP_200_OK, response_model=Message)
+def bulk_delete(ids: List[int], repo=Depends(PatientRepository)):
+    PatientService(repo).deleteMulitplePatient(ids)
+    return {"detail": "Patients delete successful."}
