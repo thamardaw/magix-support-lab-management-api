84e74792c0d4d0af11a5848d07e4298d0af4483e
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-05-10 15:25:44 +0630

    minor fix

--- core/entity/lab_test.py
+++ core/entity/lab_test.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
-# from .test_category import Test_Category
+from .test_category import Test_Category
 
 class Lab_Test(BaseModel):
     id : int
     name : str
     test_category_id : Optional[int] = None
-    # test_category : Optional[Test_Category] = None
+    test_category_ : Optional[Test_Category] = None
     created_time: Optional[datetime] = None
     updated_time: Optional[datetime] = None
     created_user_id: Optional[int] = None
