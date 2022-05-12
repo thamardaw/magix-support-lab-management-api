439a08e417f8c7778d473ba8613db521e0001656
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-05-12 16:59:47 +0630

    minor fix

--- infrastructure/models/lab_report.py
+++ infrastructure/models/lab_report.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, Integer, String, Date
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, foreign
 from infrastructure.base_mixin import BaseMixin
 from infrastructure.base_class import Base
 
 class Lab_Report(BaseMixin,Base):
     patient_id = Column(Integer,nullable=False,index=True)
+    patient = relationship('Patient', primaryjoin="Lab_Report.patient_id==foreign(Patient.id)",uselist=False)
     doctor_name = Column(String)
     sample_id = Column(Integer)
     sample_type = Column(String)
