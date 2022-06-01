57b27e2a78cb47baab4bf92296a954fc649f7439
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-06-01 22:22:56 +0630

    Minor add two field in lab test (#8)
    * minor fix
    
    * added lab report routes
    
    * minor fix
    
    * minor fixes
    
    * fix(lab_result, paramter_range): changed upper and lower limit type to float
    
    * fix(lab_test): decoupled from test category
    
    * feat(lab_test): added show in report form and request form fields
    
    * fix(env): env switch

--- core/entity/lab_test.py
+++ core/entity/lab_test.py
@@ -9,6 +9,8 @@ class Lab_Test(BaseModel):
     test_category_id : Optional[int] = None
     test_category_ : Optional[Test_Category] = None
     test_category_name : Optional[str] = None
+    show_in_request_form : Optional[bool] = None
+    show_in_report_form : Optional[bool] = None
     created_time: Optional[datetime] = None
     updated_time: Optional[datetime] = None
     created_user_id: Optional[int] = None
--- infrastructure/models/lab_test.py
+++ infrastructure/models/lab_test.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Boolean
 from sqlalchemy.orm import relationship
 from infrastructure.base_mixin import BaseMixin
 from infrastructure.base_class import Base
@@ -8,5 +8,7 @@ class Lab_Test(BaseMixin,Base):
     test_category_id = Column(Integer,index=True)
     test_category_ =  relationship('Test_Category', primaryjoin="Lab_Test.test_category_id==foreign(Test_Category.id)",uselist=False)
     test_category_name = Column(String)
+    show_in_request_form = Column(Boolean,server_default="false")
+    show_in_report_form = Column(Boolean,server_default="false")
     parameters = relationship("Parameter", backref="lab_test", passive_deletes=True)
     
